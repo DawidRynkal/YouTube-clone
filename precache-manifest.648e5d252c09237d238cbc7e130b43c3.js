ccbbfeb7f1a0f6d607f5b7f2150422e7f01aceea
Dawid Rynkal <dawidos14051997@gmail.com>
2020-07-28 18:20:52 +0200

    Updates

---
+++ precache-manifest.648e5d252c09237d238cbc7e130b43c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0225263e252768431b8e461f1e0562",
+    "url": "/YouTube-clone/index.html"
+  },
+  {
+    "revision": "f1cfc4bc8a65eb3fdb94",
+    "url": "/YouTube-clone/static/js/2.f16b60d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/YouTube-clone/static/js/2.f16b60d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2a8bc227846e225fde",
+    "url": "/YouTube-clone/static/js/main.cccbdd52.chunk.js"
+  },
+  {
+    "revision": "176e986690e109ed765e",
+    "url": "/YouTube-clone/static/js/runtime-main.4131f9ad.js"
+  }
+]);
